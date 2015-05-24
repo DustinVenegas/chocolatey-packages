623c087769a1d8a2d6e2e54241fa4c0911eabf87
Dustin Venegas <dustin.venegas@gmail.com>
2015-05-23 23:03:10 -0700

    Created figlet-go
    I found a version of figlet that works for Windows compiled against
    go-lang. Works for me!

---
+++ figlet-go/tools/chocolateyinstall.ps1
@@ -0,0 +1,1 @@
+﻿Install-ChocolateyZipPackage 'figlet-go' 'https://github.com/lukesampson/figlet/releases/download/go-1.0-win/figlet-go-1.0-win.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"

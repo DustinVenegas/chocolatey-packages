7dec2cdbbc75064c699263f87ee3ef4d91fc067c
Dustin Venegas <dustin.venegas@gmail.com>
2015-06-15 23:21:39 -0700

    Chocolatey install for Carnac

---
+++ carnac/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'carnac'
+$url = 'https://github.com/downloads/Code52/carnac/Carnac.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+# Ensure that shimgen understands this isn't a console application
+New-Item -ItemType file -force -path "$toolsDir\Carnac\Carnac.exe.gui" | out-null
+Install-ChocolateyZipPackage "$packageName" "$url" "$toolsDir"
+
+$startMenuLink=$("$env:appdata\Microsoft\Windows\Start Menu\Programs\Carnac.lnk")
+Install-ChocolateyShortcut -shortcutFilePath $startMenuLink -targetPath "$toolsDir\Carnac\Carnac.exe" | out-null
